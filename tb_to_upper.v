9d1b918675c5ddd818c2a79cea1ca62551b7cbca
Ambrose Wilkinson <ambrose.wilkins@gmail.com>
2025-11-05 19:18:43 -0500

    updated work bench so that each delay properly shows the correct variable. the function is still not fast enough

--- tb_to_upper.v
+++ tb_to_upper.v
@@ -28,103 +28,105 @@ module tb_to_upper;
 
         $display("Starting to_upper testbench...");
 
+        $display("After testing multiple delay times, #41 was found to be the correct amount to make the circuit work\n");
+
         // Test 1 ( dec 40
         test_char = 8'd40;
-        //TODO increase delay (#20) to #100 to check if the propogation delay is correct
-        #100;
+        //NOTE increase delay (#20) to #41 to check if the propogation delay is correct
+        #50;
         $display("Input: %s (%d), Output: %s (%d)", test_char, test_char, result_char, result_char);
 
         // Test 2 H dec 72
         test_char = 8'd72;
-        #100;
+        #41;
         $display("Input: %s (%d), Output: %s (%d)", test_char, test_char, result_char, result_char);
 
         // Test 3 . dec 183
         test_char = 8'd183;
-        #100;
+        #41;
         $display("Input: %s (%d), Output: %s (%d)", test_char, test_char, result_char, result_char);
 
         // Test 4 ƒ dec 131
         test_char = 8'd131;
-        #100;
+        #41;
         $display("Input: %s (%d), Output: %s (%d)", test_char, test_char, result_char, result_char);
 
         // Test 5 | dec 124
         test_char = 8'd124;
-        #100;
+        #41;
         $display("Input: %s (%d), Output: %s (%d)", test_char, test_char, result_char, result_char);
 
         // Test 6 DC4 dec 20
         test_char = 8'd20;
-        #100;
+        #41;
         $display("Input: %s (%d), Output: %s (%d)", test_char, test_char, result_char, result_char);
 
         // Test 7 ë dec 235
         test_char = 8'd235;
-        #100;
+        #41;
         $display("Input: %s (%d), Output: %s (%d)", test_char, test_char, result_char, result_char);
 
         // Test 8 a dec 97
         test_char = 8'd97;
-        #100;
+        #41;
         $display("Input: %s (%d), Output: %s (%d)", test_char, test_char, result_char, result_char);
 
         // Test 9 A dec 65
         test_char = 8'd65;
-        #100;
+        #41;
         $display("Input: %s (%d), Output: %s (%d)", test_char, test_char, result_char, result_char);
 
         // Test 10 z dec 122
         test_char = 8'd122;
-        #100;
+        #41;
         $display("Input: %s (%d), Output: %s (%d)", test_char, test_char, result_char, result_char);
 
         // Test 11 G dec 71
         test_char = 8'd71;
-        #100;
+        #41;
         $display("Input: %s (%d), Output: %s (%d)", test_char, test_char, result_char, result_char);
 
         // Test 12 m dec 109
         test_char = 8'd109;
-        #100;
+        #50;
         $display("Input: %s (%d), Output: %s (%d)", test_char, test_char, result_char, result_char);
 
         // Test 13 ' dec 146
         test_char = 8'd146;
-        #100;
+        #41;
         $display("Input: %s (%d), Output: %s (%d)", test_char, test_char, result_char, result_char);
 
         // Test 14 ) dec 48
         test_char = 8'd48;
-        #100;
+        #41;
         $display("Input: %s (%d), Output: %s (%d)", test_char, test_char, result_char, result_char);
 
         // Test 15 Ï dec 207
         test_char = 8'd207;
-        #100;
+        #41;
         $display("Input: %s (%d), Output: %s (%d)", test_char, test_char, result_char, result_char);
 
         // Test 16 : dec 58
         test_char = 8'd58;
-        #100;
+        #41;
         $display("Input: %s (%d), Output: %s (%d)", test_char, test_char, result_char, result_char);
 
         // Test 17 { dec 123
         test_char = 8'd123;
-        #100;
+        #41;
         $display("Input: %s (%d), Output: %s (%d)", test_char, test_char, result_char, result_char);
 
         // Test 18 " dec 148
         test_char = 8'd148;
-        #100;
+        #41;
         $display("Input: %s (%d), Output: %s (%d)", test_char, test_char, result_char, result_char);
 
         // Test 19 DEL dec 127
         test_char = 8'd127;
-        #100;
+        #41;
         $display("Input: %s (%d), Output: %s (%d)", test_char, test_char, result_char, result_char);
 
-        $display("Test finished.");
+        $display("\nTest finished.");
         $finish;
 end
 
